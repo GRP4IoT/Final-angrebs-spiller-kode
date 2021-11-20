cb0c0288fa045eacbcb9f0abb73721bddff30120
GRP4IoT <93915077+GRP4IoT@users.noreply.github.com>
2021-11-20 16:38:53 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,7 +3,7 @@ credentials = {
     'password' : '12345678',
     'ADAFRUIT_IO_URL' : b'io.adafruit.com',
     'ADAFRUIT_USERNAME' : b'GRP4',
-    'ADAFRUIT_IO_KEY' : b'aio_AtVw33JywUUtYgq2CMubOFSE0gYA',
+    'ADAFRUIT_IO_KEY' : b'<adafruit key>',
     'ADAFRUIT_IO_PUB_FEEDNAME' : b'bot_pub',
     'ADAFRUIT_IO_SUB_FEEDNAME' : b'distanceforsvar',
     'ADAFRUIT_IO_SUB_FEEDNAME2' : b'rssi',
